983005b01bf72bc7f86bd400aac2e8297bad3122
Mohammad Asif <asif332@gmail.com>
2019-09-12 12:33:45 +0530

    initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Asif on 9/12/19.
+//  Copyright © 2019 Niit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
